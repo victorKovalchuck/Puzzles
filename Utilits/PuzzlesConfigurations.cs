c9342f825d5628205f7211480b8e47a0ae73cdf3
victorKovalchuck <victorkovalchuck@gmail.com>
2015-11-02 17:59:28 +0200

    modifed

---
+++ Utilits/PuzzlesConfigurations.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Utilits
+{
+    public class PuzzlesConfigurations
+    {        
+        public enum puzzleBrakeCoupleEnum { horizontalDivide, verticalCouple, untouchable, verticalDivide, horizontalCouple };
+        public enum rotateVariants { Rotate90, Rotate180, Rotate270 };
+    
+    }
+}
