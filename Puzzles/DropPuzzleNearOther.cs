6b69b70c7a114fc5bb6b45f957c9a1cf7291c5c0
victorKovalchuck <victorkovalchuck@gmail.com>
2015-11-02 17:59:13 +0200

    none

---
+++ Puzzles/DropPuzzleNearOther.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Utilits;
+
+namespace Puzzles
+{
+    public class DropPuzzleNearOther
+    {
+        Puzzle _droppingPuzzle;
+        public DropPuzzleNearOther(Puzzle droppingPuzzle)
+        {
+            _droppingPuzzle = droppingPuzzle;
+        }
+
+
+        public void PlacePuzzles(List<Puzzle> puzzles)
+        {
+            GetClosestPuzzle(puzzles);        
+        }
+
+        private void GetClosestPuzzle(List<Puzzle> puzzles)
+        { 
+        
+        }
+    }
+}
